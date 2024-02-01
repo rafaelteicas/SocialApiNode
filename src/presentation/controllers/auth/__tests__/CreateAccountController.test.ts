44307395499acc5c503a4346760fd8d40073da0e
Rafael <rafaelteicas@gmail.com>
2024-02-01 13:44:23 -0300

    test: CreateAccountController should return badRequest response

--- src/presentation/controllers/auth/__tests__/CreateAccountController.test.ts
+++ src/presentation/controllers/auth/__tests__/CreateAccountController.test.ts
@@ -1,15 +1,17 @@
-import { type CreateAccountType } from '../../../../domain/CreateAccountType'
+import { type AccountModel } from '../../../../domain/models/AccountModel'
+import { type EmailValidatorUseCase } from '../../../../domain/usecases/EmailValidatorUseCase'
 import { type Controller } from '../../ControllerType'
 import { CreateAccountController } from '../CreateAccountController'
 
 interface SutTypes {
   sut: Controller
+  emailValidatorStub: EmailValidatorUseCase
 }
 
 const data = '14/06/2002'
 const splitData = data.split('/')
 
-const mockedAccount: CreateAccountType = {
+const mockedAccount: AccountModel = {
   id: 1,
   birthday: new Date(
     parseInt(splitData[0]),
@@ -22,10 +24,21 @@ const mockedAccount: CreateAccountType = {
   username: 'username'
 }
 
+function makeEmailValidator (): EmailValidatorUseCase {
+  class EmailValidatorStub implements EmailValidatorUseCase {
+    validate (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
 function makeSut (): SutTypes {
-  const sut = new CreateAccountController()
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new CreateAccountController(emailValidatorStub)
   return {
-    sut
+    sut,
+    emailValidatorStub
   }
 }
 
@@ -33,6 +46,17 @@ describe('CreateAccountController', () => {
   it('should return BadRequest if missing params', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({ body: { ...mockedAccount.birthday } })
+    expect(httpResponse).toEqual({
+      body: 'Bad request',
+      statusCode: 400
+    })
+  })
+  it('should validate email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'validate').mockReturnValueOnce(false)
+    const httpResponse = await sut.handle({
+      body: { ...mockedAccount, email: 'fake_email' }
+    })
     expect(httpResponse).toEqual({
       body: 'Bad request',
       statusCode: 400
