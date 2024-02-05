54280e487362f7c9c3daa2cc22c3e4f3d707387f
Rafael <rafaelteicas@gmail.com>
2024-02-05 14:42:52 -0300

    refactor: remove RepositoryType

--- src/data/auth/CreateAccountRepository.ts
+++ src/data/auth/CreateAccountRepository.ts
@@ -1,8 +1,9 @@
+import { type Repository } from 'typeorm'
 import { type AccountModel } from '../../domain/models/AccountModel'
-import { type Repository } from '../RepositoryType'
+import { type AccountEntity } from '../../infra/orm/entities/AccountEntity'
 
 export class CreateAccountRepository {
-  constructor (private readonly userRepository: Repository) { };
+  constructor (private readonly userRepository: Repository<AccountEntity>) { };
 
   create (account: AccountModel): void {
     this.userRepository.save(account)
