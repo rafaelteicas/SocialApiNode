391703a79091aa75bf0aef4d72f53cc8d913772d
Rafael <rafaelteicas@gmail.com>
2024-02-05 19:15:11 -0300

    test: implement CreateAccount repository

--- src/presentation/controllers/auth/__tests__/CreateAccountController.test.ts
+++ src/presentation/controllers/auth/__tests__/CreateAccountController.test.ts
@@ -1,4 +1,5 @@
 import { type AccountModel } from '../../../../domain/models/AccountModel'
+import { type CreateAccountUseCase } from '../../../../domain/usecases/CreateAccountUseCase'
 import { type EmailValidatorUseCase } from '../../../../domain/usecases/EmailValidatorUseCase'
 import { BadRequestError } from '../../../helpers/errors/BadRequestError'
 import { MissingParamError } from '../../../helpers/errors/MissingParamError'
@@ -6,11 +7,6 @@ import { ServerError } from '../../../helpers/errors/ServerError'
 import { DefaultResponses } from '../../../helpers/responses/DefaultResponses'
 import { type Controller } from '../../ControllerType'
 import { CreateAccountController } from '../CreateAccountController'
-
-interface SutTypes {
-  sut: Controller
-  emailValidatorStub: EmailValidatorUseCase
-}
 
 const data = '14/06/2002'
 const splitData = data.split('/')
@@ -27,21 +23,39 @@ const mockedAccount: AccountModel = {
   username: 'username'
 }
 
-function makeEmailValidator (): EmailValidatorUseCase {
-  class EmailValidatorStub implements EmailValidatorUseCase {
+function makeCreateAccountUseCase (): CreateAccountUseCase {
+  class CreateAccountUseCaseStub implements CreateAccountUseCase {
+    async create (account: AccountModel): Promise<void> {}
+  }
+  return new CreateAccountUseCaseStub()
+}
+
+function makeEmailValidatorUseCase (): EmailValidatorUseCase {
+  class EmailValidatorUseCaseStub implements EmailValidatorUseCase {
     validate (email: string): boolean {
       return true
     }
   }
-  return new EmailValidatorStub()
+  return new EmailValidatorUseCaseStub()
+}
+
+interface SutTypes {
+  sut: Controller
+  emailValidatorUseCaseStub: EmailValidatorUseCase
+  createAccountUseCaseStub: CreateAccountUseCase
 }
 
 function makeSut (): SutTypes {
-  const emailValidatorStub = makeEmailValidator()
-  const sut = new CreateAccountController(emailValidatorStub)
+  const createAccountUseCaseStub = makeCreateAccountUseCase()
+  const emailValidatorUseCaseStub = makeEmailValidatorUseCase()
+  const sut = new CreateAccountController(
+    emailValidatorUseCaseStub,
+    createAccountUseCaseStub
+  )
   return {
     sut,
-    emailValidatorStub
+    emailValidatorUseCaseStub,
+    createAccountUseCaseStub
   }
 }
 
@@ -54,8 +68,10 @@ describe('CreateAccountController', () => {
     expect(httpResponse).toEqual(responses.error(new MissingParamError('email')))
   })
   it('should validate email', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'validate').mockReturnValueOnce(false)
+    const { sut, emailValidatorUseCaseStub } = makeSut()
+    jest.spyOn(emailValidatorUseCaseStub, 'validate').mockReturnValueOnce(
+      false
+    )
     const httpResponse = await sut.handle({
       body: { ...mockedAccount, email: 'fake_email' }
     })
@@ -75,12 +91,24 @@ describe('CreateAccountController', () => {
     })
     expect(httpResponse).toEqual(responses.error(new BadRequestError()))
   })
-  it('should throw if email validator throws', async () => {
-    const { sut, emailValidatorStub } = makeSut()
-    jest.spyOn(emailValidatorStub, 'validate').mockImplementationOnce(() => { throw new Error() })
+  it('should throw if EmailValidatorUseCase throws', async () => {
+    const { sut, emailValidatorUseCaseStub } = makeSut()
+    jest.spyOn(emailValidatorUseCaseStub, 'validate').mockImplementationOnce(
+      () => {
+        throw new Error()
+      }
+    )
     const httpResponse = await sut.handle({
       body: { ...mockedAccount }
     })
     expect(httpResponse).toEqual(responses.error(new ServerError()))
+  })
+  it('should call CreateAccountUseCase with correct values', async () => {
+    const { sut, createAccountUseCaseStub } = makeSut()
+    const createUseCase = jest.spyOn(createAccountUseCaseStub, 'create')
+    sut.handle({
+      body: { ...mockedAccount }
+    })
+    expect(createUseCase).toHaveBeenCalledWith(mockedAccount)
   })
 })
